762ac5ac84477c850d62795cd672659fa1bcc64e
carminesorrentino <c.sorrentino50@studenti.unisa.it>
2021-07-01 15:59:03 +0200

    routing

--- covidUSAbd2/src/app/app-routing.module.ts
+++ covidUSAbd2/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './features/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'homepage', component: HomepageComponent },
+  { path:'**', redirectTo:'/homepage'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- covidUSAbd2/src/app/app.module.ts
+++ covidUSAbd2/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeaturesModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
